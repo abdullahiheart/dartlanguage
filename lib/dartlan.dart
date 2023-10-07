3c7bee54946771bcd047e30d2b230cb8f85a759c
abdullahi Hear <abdalaheart@gmail.com>
2023-10-07 14:04:12 +0300

    first

--- lib/dartlan.dart
+++ lib/dartlan.dart
@@ -1,3 +1,7 @@
 void main() {
-  print('Hello World');
+  //print('Hello World');//Hard-coded String
+  //Variable = Waasida weel oo kalle boos ayaa laguu qawanaa memory gaar ahan ram ka
+  String greeting = 'Hello word';
+  print(greeting);
+
 }
