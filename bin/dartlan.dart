ef697a75f85cf46f43febf73a3edc4bb2a924937
abdullahi Hear <abdalaheart@gmail.com>
2023-10-07 10:30:06 +0300

    first commit

---
+++ bin/dartlan.dart
@@ -0,0 +1,5 @@
+import 'package:dartlan/dartlan.dart' as dartlan;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartlan.calculate()}!');
+}
---
+++ test/dartlan_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartlan/dartlan.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
