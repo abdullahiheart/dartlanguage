9dce525399f10d59bff8648ee417e363ff483931
abdullahi Hear <abdalaheart@gmail.com>
2023-10-09 08:53:17 +0300

    Variable deep Explanation

--- lib/dartlan.dart
+++ lib/dartlan.dart
@@ -1,7 +1,42 @@
 void main() {
+  /*//Explicity Declared Variable waa marka ay cadahe  | nocyada langua prog waxaa ladhahaa Type-safe
   //print('Hello World');//Hard-coded String
-  //Variable = Waasida weel oo kalle boos ayaa laguu qawanaa memory gaar ahan ram ka
   String greeting = 'Hello word';
   print(greeting);
+  //Variable = Waasida weel oo kalle boos ayaa laguu qawanaa memory gaar ahan ram ka
+  //Basic Data type in Dart: String, Booleans, Numbers
+  //lists(Arrays), Maps(json)
 
+  String Greeting = 'Hello World';
+
+  bool isNew = true;
+  print(isNew);
+  // int and double
+  int numberoffouts = 45;
+  double amount = 19.99;
+  // waxa uu kuu suurto galinee in double ama int midkii larawo lasoo galin karo
+  num  price =12.5;
+  num pricetwo = 12;
+  //list markii lajogo Dart Aray cml loo isticmalaa
+  //np list same data type wee noqon karan weena kala duwanan karan
+  //lists Aad Ayuu Muhiim Uyahe most waa la isticmalaa applicatio badana waa lagu dhisaa Sida Quizs cml
+  List listofstudents = ['Abdi', 'Ali', 'Aisha'];
+  // print(listofstudents[2]);
+  listofstudents.forEach((e) {
+    print(e);
+  });
+  //Mapps waxa uu udhigmaa Sida jyson cml waxa uu ka kowan yahy k iyo value
+  //waxa ay ushaqene Key-value pairs
+  Map<String, dynamic> personalinfo = {
+    'name': 'Abdi farah ali',
+    'phone': '61xxxxx'
+  };
+  print(personalinfo['phone' ]);*/
+  //Implicity Declared Variables Hadii Adan aqoon Noca data type nooca ay tahy ayaa ladhahaa |
+  //var and dynamic Farqiga Udhaxeyo waa Var hadii ad string u istic masho another data tyape kalle uma istic mali kartid
+  //dynamic waxa uu kaka duwan yahy waa markasta data type waa isbadali kartaa string adoo u istic malle Hadane int waad u istic mali kartaa
+  // var greeting = 'abdillahi';
+  // print(greeting.runtimeType);
+  dynamic greatingthree = 'Hello word';
+  print(greatingthree);
 }
